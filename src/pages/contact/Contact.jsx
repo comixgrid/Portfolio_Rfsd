9959fa90eff11ebc7f492f3608d2e97140df576d
techeduhelper <uccmaniruddinkhan@gmail.com>
2023-09-21 16:59:38 +0530

    fixed

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -36,58 +36,61 @@ const Contact = ({ isActive }) => {
   };
 
   return (
-    <div
-      className="contact-page mobile-view visible-lg overflow-x-hidden d-flex  flex-column justify-content-center align-items-center"
-      style={{ display: isActive ? "block" : "none" }}
-      id="contact"
-    >
-      <div className="contact-container d-flex flex-column w-100 justify-content-center align-items-center ">
-        <form
-          onSubmit={handleSubmit}
-          className="form-container d-flex flex-column justify-content-center gap-3 w-75 col-md-12 mb-5"
+    <>
+      {isActive && (
+        <div
+          className="contact-page mobile-view visible-lg overflow-x-hidden d-flex  flex-column justify-content-center align-items-center"
+          style={{ display: isActive ? "block" : "none" }}
         >
-          <Jump>
-            <p>Contact Me👍</p>
-          </Jump>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              placeholder="Enter your name"
-              className="form-control "
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+          <div className="contact-container d-flex flex-column w-100 justify-content-center align-items-center ">
+            <form
+              onSubmit={handleSubmit}
+              className="form-container d-flex flex-column justify-content-center gap-3 w-75 col-md-12 mb-5"
+            >
+              <Jump>
+                <p>Contact Me👍</p>
+              </Jump>
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="form-control "
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  className="form-control "
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id=""
+                  cols="30"
+                  rows="6"
+                  className="form-control p-2"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-danger p-2 fs-4 ">
+                Send Message
+              </button>
+            </form>
           </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              placeholder="Enter Your Email"
-              className="form-control "
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id=""
-              cols="30"
-              rows="6"
-              className="form-control p-2"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-danger p-2 fs-4 ">
-            Send Message
-          </button>
-        </form>
-      </div>
-      <div className="mt-5">All rights reserved techeduhelper @2023</div>
-    </div>
+          <div className="mt-5">All rights reserved techeduhelper @2023</div>
+        </div>
+      )}
+    </>
   );
 };
 
